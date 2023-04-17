d92613902c530db0df26169c45cf36057c358500
Edgard <20133252@ealoe.ulima.edu.pe>
2023-04-17 09:30:52 -0500

    Initial Commit

---
+++ loginExampleODS/loginExampleODSApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginExampleODSApp.swift
+//  loginExampleODS
+//
+//  Created by EdgardVS on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct loginExampleODSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
